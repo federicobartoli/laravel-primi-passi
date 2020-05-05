4c31bcf3c7d1c05215df89e4cf6a19f09c456a68
Federico <federicobartoli@me.com>
2020-05-05 15:06:55 +0200

    Commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+     <div class="lamolisana-header">
+          <div class="logo">
+               <img src="{{asset('img/logo.png')}}" alt="logo molisana">
+          </div>
+          <nav>
+               <ul>
+                    <li><a href="#">Home</a></li>
+                    <li class="active"><a href="{{route('prodotti')}}">Prodotti</a></li>
+                    <li><a href="#">News</a></li>
+
+               </ul>
+          </nav>
+     </div>
+</header>
